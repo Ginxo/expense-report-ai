f2dda7066a12f5434e430bc02add1840655fdd2c
Enrique Mingorance Cano <emingora@redhat.com>
2023-05-31 20:28:16 +0200

    Static content (#6)
    * deploy preparation
    
    * deploy_to_netlify just for main branch
    
    * static content preparation

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Auth0ProviderWithNavigate } from "./shared/security/Auth0ProviderWithNavigate";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Auth0ProviderWithNavigate>
         <App />
       </Auth0ProviderWithNavigate>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
